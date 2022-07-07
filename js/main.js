b0ee1592a9f30e173ab2309e2c36d086ec31669f
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-07-07 16:01:31 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  slideClass: "swiper-slide",
+  loop: true,
+  slideClass: "swiper-slide",
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
+// let mySwiper;
+// mySwiper = new Swiper(slider, {
+//     // Optional parameters
+//    slidesPerView:4,
+//     loop: true,
+//     slideClass: "swiper-slide",
+//     spaceBetween: 30,
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+
+//   });
