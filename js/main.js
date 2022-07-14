42afe05b4eff38542f6aac1400efe326f9c87104
ermuxanov <74331498+ermuxanov@users.noreply.github.com>
2022-07-14 20:57:29 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,23 +1,46 @@
-new Swiper('.swiper', {
+let aboutSlider = document.querySelector('.about-swiper');
+
+
+new Swiper(aboutSlider, {
   slidesPerView: 4,
   spaceBetween: 30,
   slideClass: "swiper-slide",
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.about-next',
+    prevEl: '.about-prev',
   },
 })
-new Swiper('.publications-swiper', {
+
+const slider = document.querySelector('.publications-swiper')
+const partnerSlider = document.querySelector('.partner-swiper')
+
+
+let mySwiper = new Swiper(slider, {
   slidesPerView: 3,
   spaceBetween: 30,
-  slideClass: "swiper-slide",
+  slideClass: "publications__item",
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.publications-next',
+    prevEl: '.publications-prev',
   },
 })
+
+let partnerSwiper = new Swiper(partnerSlider, {
+  slidesPerView: 6,
+
+  spaceBetween: 30,
+  slideClass: "partner__slide",
+  loop: true,
+  navigation: {
+    nextEl: '.partner-next',
+    prevEl: '.partner-prev',
+  },
+})
+
+
+
 // let mySwiper;
 // mySwiper = new Swiper(slider, {
 //     // Optional parameters
